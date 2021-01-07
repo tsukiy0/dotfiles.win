d86b8477d51ce9d8db5a2bafc71dc1ea22a43382
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-01-07 21:24:47 +1100

    feat: install apps

---
+++ scripts/apps.ps1
@@ -0,0 +1,16 @@
+# cmdline
+choco install -y git
+choco install -y nvm
+choco install -y dotnet-5.0-sdk
+choco install -y awscli
+choco install -y vim
+
+# gui
+choco install -y microsoft-windows-terminal
+choco install -y googlechrome
+choco install -y vscode
+choco install -y powertoys
+choco install -y docker-desktop
+choco install -y vlc
+choco install -y spotify
+choco install -y steam
