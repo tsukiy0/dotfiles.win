f256bf6c264740dec58f1df0385d49af0abc3d6d
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-01-07 21:56:48 +1100

    feat: configure keyboard repeat

--- scripts/config.ps1
+++ scripts/config.ps1
@@ -1,1 +1,5 @@
 Copy-Item -Path $PSScriptRoot/../home/** -Destination $HOME -Include **
+
+# keyboard repeat
+reg add "HKCU\Control Panel\Keyboard" /v KeyboardDelay /t REG_SZ /d "0" /f
+reg add "HKCU\Control Panel\Keyboard" /v KeyboardSpeed /t REG_SZ /d "31" /f
