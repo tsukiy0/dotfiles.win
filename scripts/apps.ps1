d3acc8b0522559bad093e09ab04a66b04ed0387a
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-02-17 23:40:00 +1100

    feat: install transmission

--- scripts/apps.ps1
+++ scripts/apps.ps1
@@ -20,6 +20,7 @@ choco install -y steam
 choco install -y postman
 choco install -y jetbrainstoolbox
 choco install -y dbeaver
+choco install -y transmission
 
 # jabba
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
