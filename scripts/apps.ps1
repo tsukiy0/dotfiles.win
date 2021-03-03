723154c51ccbd098d12e113241b56928aaa26832
tsukiy0 <tsukiy0@users.noreply.github.com>
2021-03-03 16:25:38 +1100

    feat: add autohotkey

--- scripts/apps.ps1
+++ scripts/apps.ps1
@@ -21,6 +21,7 @@ choco install -y jetbrainstoolbox
 choco install -y dbeaver
 choco install -y transmission
 choco install -y slack
+choco install -y autohotkey
 
 # jabba
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
